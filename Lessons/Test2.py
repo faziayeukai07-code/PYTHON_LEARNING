6e8610e27f23432c91e0c23ba1484d985fda9814
Fazia <faziayeukai07@gmail.com>
2026-09-17 00:48:16 +0100

    second commit

---
+++ Lessons/Test2.py
@@ -0,0 +1,5 @@
+age = 20
+has_ticket = False
+print(age >= 18 and has_ticket) #and represents both conditions to apply
+print(age < 18 or has_ticket)# or represents either on condition to apply
+print(not has_ticket)
